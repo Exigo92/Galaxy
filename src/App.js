2af784c1a2844988126e3201cb46362a50200ae3
Exigo92 <91794037+Exigo92@users.noreply.github.com>
2022-11-28 18:25:33 +0100

    Commit1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Training from './pages/Training';
+import Pricing from './pages/Pricing';
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/training' element={<Training />} />
+          <Route path='/prices' element={<Pricing />} />
+          <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
